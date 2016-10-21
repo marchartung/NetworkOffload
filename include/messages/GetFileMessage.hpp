5d4c5e97d026cd2467ecf238c9e912df27ae4dfe
mflehmig <martin.flehmig@tu-dresden.de>
2016-10-21 14:59:00 +0200

    When using make shared to create arrays we need to make sure, that delete[] is called

--- include/messages/GetFileMessage.hpp
+++ include/messages/GetFileMessage.hpp
@@ -8,12 +8,15 @@
 #ifndef INCLUDE_MESSAGES_GETFILEMESSAGE_HPP_
 #define INCLUDE_MESSAGES_GETFILEMESSAGE_HPP_
 
-#include <fstream>
-#include <algorithm>
-
 #include "AbstractMessage.hpp"
-#include "../AdditionalTypes.hpp"
-#include "../network_impl/SimNetworkFunctions.hpp"
+#include "AdditionalTypes.hpp"
+#include "network_impl/SimNetworkFunctions.hpp"
+
+#include <algorithm>
+#include <fstream>
+#include <iostream>
+#include <memory>
+#include <string>
 
 namespace NetOff
 {
@@ -30,13 +33,13 @@ namespace NetOff
         {
             std::cout << "size: " << _dataSize << "\n";
             _dataSize = sizeof(InitialClientMessageSpecifyer) + sizeof(int) + getStringDataSize(file);
-            _data = std::shared_ptr<char>(new char[_dataSize]);
+            _data = std::shared_ptr<char>(new char[_dataSize], std::default_delete<char[]>());
             _req = reinterpret_cast<InitialClientMessageSpecifyer*>(_data.get());
             char * p = saveShiftIntegralInData(InitialClientMessageSpecifyer::GET_FILE, _data.get());
             _simId = reinterpret_cast<int*>(p);
             p = saveShiftIntegralInData<int>(simId, p);
             _path = p;
-            saveStringInData(file,_path);
+            saveStringInData(file, _path);
         }
 
         GetFileMessage(std::shared_ptr<char> & data)
@@ -55,6 +58,7 @@ namespace NetOff
         }
 
         GetFileMessage(const GetFileMessage& rhs) = delete;
+
         GetFileMessage& operator=(const GetFileMessage& rhs) = delete;
 
         ~GetFileMessage()
@@ -99,31 +103,31 @@ namespace NetOff
     {
      public:
         GetFileSuccessMessage(const int & simId, const std::string & file)
-            : AbstractMessage<InitialServerMessageSpecifyer>(InitialServerMessageSpecifyer::SUCCESS_GET_FILE),
-              _data(nullptr),
-              _dataSize(0),
-              _req(nullptr),
-              _simId(nullptr),
-              _fileStart(nullptr)
+                : AbstractMessage<InitialServerMessageSpecifyer>(InitialServerMessageSpecifyer::SUCCESS_GET_FILE),
+                  _data(nullptr),
+                  _dataSize(0),
+                  _req(nullptr),
+                  _simId(nullptr),
+                  _fileStart(nullptr)
         {
             std::ifstream fileStream;
             std::streampos startPos, endPos;
             fileStream.open(file.c_str(), std::ios::binary);
             startPos = fileStream.tellg();
-            fileStream.seekg(0,std::ios::end);
+            fileStream.seekg(0, std::ios::end);
             endPos = fileStream.tellg();
 
             _dataSize = sizeof(InitialServerMessageSpecifyer) + sizeof(int) + endPos - startPos;
-            _data = std::shared_ptr<char>(new char[_dataSize]);
+            _data = std::shared_ptr<char>(new char[_dataSize], std::default_delete<char[]>());
             _req = reinterpret_cast<InitialServerMessageSpecifyer*>(_data.get());
-            char * p = saveShiftIntegralInData(InitialServerMessageSpecifyer::SUCCESS_GET_FILE,_data.get());
+            char * p = saveShiftIntegralInData(InitialServerMessageSpecifyer::SUCCESS_GET_FILE, _data.get());
             _simId = reinterpret_cast<int*>(p);
-            p = saveShiftIntegralInData(simId,p);
+            p = saveShiftIntegralInData(simId, p);
             _fileStart = p;
-            fileStream.seekg(0,std::ios::beg);
+            fileStream.seekg(0, std::ios::beg);
             size_t count = sizeof(InitialServerMessageSpecifyer) + sizeof(int);
             size_t blockLength = 512, curReadLength = 1;
-            while(fileStream.good() && curReadLength > 0)
+            while (fileStream.good() && curReadLength > 0)
             {
                 curReadLength = std::min(blockLength, _dataSize - count);
                 fileStream.read(p, curReadLength);
@@ -131,17 +135,17 @@ namespace NetOff
                 count += curReadLength;
             }
             fileStream.close();
-            if(count != _dataSize)// || endPos != fileStream.tellg())
+            if (count != _dataSize)  // || endPos != fileStream.tellg())
                 throw std::runtime_error("GetFileSuccessMessage: Error during file read.");
         }
 
         GetFileSuccessMessage(const size_t numBytes, std::shared_ptr<char> & data, const std::string & file)
-        : AbstractMessage<InitialServerMessageSpecifyer>(InitialServerMessageSpecifyer::SUCCESS_GET_FILE),
-          _data(data),
-          _dataSize(numBytes),
-          _req(nullptr),
-          _simId(nullptr),
-          _fileStart(nullptr)
+                : AbstractMessage<InitialServerMessageSpecifyer>(InitialServerMessageSpecifyer::SUCCESS_GET_FILE),
+                  _data(data),
+                  _dataSize(numBytes),
+                  _req(nullptr),
+                  _simId(nullptr),
+                  _fileStart(nullptr)
         {
             _req = reinterpret_cast<InitialServerMessageSpecifyer*>(_data.get());
             char * p = _data.get();
@@ -152,7 +156,7 @@ namespace NetOff
             size_t curReadLength = 1, count = sizeof(InitialServerMessageSpecifyer) + sizeof(int), blockLength = 512;
             std::ofstream fileStream;
             fileStream.open(file.c_str(), std::ios::binary);
-            while(fileStream.good() && curReadLength > 0)
+            while (fileStream.good() && curReadLength > 0)
             {
                 curReadLength = std::min(blockLength, _dataSize - count);
                 fileStream.write(p, curReadLength);
@@ -163,6 +167,7 @@ namespace NetOff
         }
 
         GetFileSuccessMessage(const GetFileSuccessMessage& rhs) = delete;
+
         GetFileSuccessMessage& operator=(const GetFileSuccessMessage& rhs) = delete;
 
         ~GetFileSuccessMessage()
